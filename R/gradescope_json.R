5ee427618477104c0cf76f30db7e4b2ccd53c16e
Derek Friend <dfriend@dordt.edu>
2026-08-17 23:02:20 -0500

    fix error in gradescope_json()

--- R/gradescope_json.R
+++ R/gradescope_json.R
@@ -12,7 +12,6 @@ gradescope_json <- function(df, ...) {
   names(df)[match(names(names_dict), names(df))] <- names_dict
   rownames(df) <- NULL
   lst <- list(
-    time = tm["elapsed"],
     visibility = "after_due_date",
     stdout_visibility = "visible",
     ...,
